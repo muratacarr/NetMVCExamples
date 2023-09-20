4d32d05ca3e022c0aae6a4463a4c95c030bd1cf0
Yavuz Selim KAHRAMAN <yskahraman@gmail.com>
2023-09-20 21:19:55 +0300

    completed

---
+++ Example01/Controllers/PartialController.cs
@@ -0,0 +1,24 @@
+﻿using Example01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example01.Controllers
+{
+    public class PartialController : Controller
+    {
+        // GET: Partial
+        public ActionResult Products()
+        {
+            var product = new Product
+            {
+                Name = "Samsung S21 5g",
+                Price = 15000
+            };
+            return View(product);
+        }
+    }
+}
